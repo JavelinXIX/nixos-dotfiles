7d83693fdede8f6c464f201b1428bade142b0660
tomoya-f <asgard.fortissimo.1125@gmail.com>
2024-01-27 01:22:08 +0000

    add two alias

--- wsl/zsh.nix
+++ wsl/zsh.nix
@@ -3,7 +3,8 @@
     enable = true;
     shellAliases = {
       ll = "ls -l";
-      update = "sudo nixos-rebuild switch --flake ~/.dotfiles/flake.nix#nixos";
+      flake-update = "sudo nixos-rebuild switch --flake ~/.dotfiles/flake.nix#nixos --impure";
+      homemanager-update = "nix run home-manager -- switch --flake .#home"
     };
     oh-my-zsh = {
       enable = true;
