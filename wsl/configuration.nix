a41d0b2c922698f0d9855c394f69df1356694357
tomoya-f <asgard.fortissimo.1125@gmail.com>
2024-01-27 01:23:56 +0000

    fix

--- wsl/configuration.nix
+++ wsl/configuration.nix
@@ -8,53 +8,60 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [
-    # include NixOS-WSL modules
-    <nixos-wsl/modules>
-  ];
+	imports = [
+		# include NixOS-WSL modules
+		<nixos-wsl/modules>
+	];
 
-  wsl.enable = true;
-  wsl.defaultUser = "tomoya-f";
-  
+	wsl.enable = true;
+	wsl.defaultUser = "tomoya-f";
+	
 
-    users.users.tomoya-f = {
-    isNormalUser = true;
-    extraGroups = ["wheel"];
-    home = "/home/tomoya-f";
-    shell = pkgs.zsh;
-  };
+		users.users.tomoya-f = {
+		isNormalUser = true;
+		extraGroups = ["wheel", "docker"];
+		home = "/home/tomoya-f";
+		shell = pkgs.zsh;
+	};
 
-  # enabled programs
-  programs = {
-    zsh = {
-      enable = true;
-    };
-    neovim = {
-      enable = true;
-      defaultEditor = true;
-      viAlias = true;
-      vimAlias = true;
-    };
-    git = {
-      enable = true;
-    };
-    tmux = {
-      enable = true;
-    };
-  };
+	# enabled programs
+	programs = {
+		zsh = {
+			enable = true;
+		};
+		neovim = {
+			enable = true;
+			defaultEditor = true;
+			viAlias = true;
+			vimAlias = true;
+		};
+		git = {
+			enable = true;
+		};
+		tmux = {
+			enable = true;
+		};
+	};
 
-  # enabled system packages
-  environment.systemPackages = with pkgs; [
-    unzip
-    p7zip
-  ];
+	# enabled system packages
+	environment.systemPackages = with pkgs; [
+		unzip
+		p7zip
+	];
  
- 
-  nix = {
-    settings = {
-      experimental-features = ["nix-command" "flakes"];
+	# Docker
+	virtualisation.docker.enable = true;
+    virtualisation.docker.rootless = {
+	    enable = true;
+	    setSocketVariable = true;
     };
-  };
 
-  system.stateVersion = "23.11"; # Did you read the comment?
+
+	nix = {
+		settings = {
+			experimental-features = ["nix-command" "flakes"];
+		};
+	};
+
+	system.stateVersion = "23.11"; # Did you read the comment?
 }
