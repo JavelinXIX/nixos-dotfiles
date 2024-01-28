ea48cefd543c61d0ff4215a776c439a8e8f0b49d
tomoya-f <asgard.fortissimo.1125@gmail.com>
2024-01-28 09:25:59 +0000

    fix path

--- wsl/zsh.nix
+++ wsl/zsh.nix
@@ -3,7 +3,7 @@
     enable = true;
     shellAliases = {
       ll = "ls -l";
-      flake-update = "sudo nixos-rebuild switch --flake ~/.dotfiles/flake.nix#nixos --impure";
+      flake-update = "sudo nixos-rebuild switch --flake ~/.dotfiles/wsl/flake.nix#nixos --impure";
       homemanager-update = "nix run home-manager -- switch --flake .#home";
     };
     oh-my-zsh = {
